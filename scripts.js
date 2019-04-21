2b891f3a8656eafd3691a2d2b1a1b7a543837203
Miranda W <36822462+mirandalwashburn@users.noreply.github.com>
2019-04-21 11:39:03 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+$('#main').toggleClass(localStorage.toggled);
+
+function darkLight() {
+  /*DARK CLASS*/
+  if (localStorage.toggled != 'dark') {
+    $('#main, p').toggleClass('dark', true);
+    localStorage.toggled = "dark";
+     
+  } else {
+    $('#main, p').toggleClass('dark', false);
+    localStorage.toggled = "";
+  }
+}
+
+/*Add 'checked' property to input if background == dark*/
+if ($('main').hasClass('dark')) {
+   $( '#checkBox' ).prop( "checked", true )
+} else {
+  $( '#checkBox' ).prop( "checked", false )
+}
+
